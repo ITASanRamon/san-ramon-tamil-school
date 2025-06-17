7e7d212fec796f6e856172ff9ff7cdcc243b45a6
Alexandre Mendes <alexandregme@gmail.com>
2025-06-17 08:20:04 -0700

    external links

--- app/sections/header.tsx
+++ app/sections/header.tsx
@@ -4,27 +4,30 @@ const Header = () => {
     link:"/"
   },{
     name:"Our School",
-    link:"/"
+    href:"/"
   },
     {
       name:"School Resgistration",
-      link:"/"
+      href:"https://www.catamilacademy.org/registration.html",
+      target:"_blank"
     },
     {
       name:"HSCP Registration",
-      link:"/"
+      href:"https://catamilacademy.org/CreditProgram.html",
+      target:"_blank"
     },
     {
       name:"Contact Us",
-      link:"/"
+      href:"/"
     }]
   return(
     <nav>
       <ul className="flex">
           {links.map(
               (link, index) => {
-                return(<li className="links m-5 w-100 h-10 cursor-pointer content-center text-center border-rounded" key={index}>
-                  <span>{link.name}</span></li>)
+                return(
+
+                  <a href={link.href} target={link.target || "_self"} className="links m-5 w-100 h-10 cursor-pointer content-center text-center border-rounded"  key={index}>{link.name}</a>)
               }
           )}
       </ul>
